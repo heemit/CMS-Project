630f5a87df08bada7c72b2c8cfa0ed3de0fd6622
Heemit Shah <32201768+heemit@users.noreply.github.com>
2025-03-24 03:25:20 +0530

    Update urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -2,10 +2,6 @@ from django.contrib             import admin
 from django.urls                import path, include
 from django.conf                import settings
 from django.conf.urls.static    import static
-from django.http                import HttpResponseRedirect
-
-def redirect_admin(request):
-    return HttpResponseRedirect('http://inland-gerri-heemit-0a6c5f26.koyeb.app/admin/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
